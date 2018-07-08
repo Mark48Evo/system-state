af43bc1ac2421117f43f52bd2218fae2e361c607
Aurimas Niekis <aurimas@niekis.lt>
2018-07-08 18:54:21 +0100

    Initial Commit

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  {
+    input: pkg.module,
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+    ],
+    external: [
+      '@mark48evo/rabbitmq-pubsub',
+      'debug',
+      'events',
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        envName: 'rollup',
+      }),
+    ],
+  },
+];
