610953efd4c6e701daa61041c4a703c3d1a32c34
Aurimas Niekis <aurimas@niekis.lt>
2018-07-08 19:10:43 +0100

    Made a construction function

--- src/index.js
+++ src/index.js
@@ -1,83 +1,13 @@
-import Debug from 'debug';
-import { EventEmitter } from 'events';
-import RabbitMQPubSub from '@mark48evo/rabbitmq-pubsub';
+import SystemState from './systemState';
 
-const debug = Debug('system:state');
+/**
+ * @param {Redis} redis
+ * @param {Channel} channel
+ * @returns {Promise<RabbitMQPubSub>}
+ */
+export default async function (redis, channel) {
+  const systemState = new SystemState(redis, channel);
+  await systemState.setup();
 
-const config = {
-  exchangeName: process.env.EXCHANGE_NAME || 'system_state',
-  queueNamePrefix: process.env.QUEUE_NAME_PREFIX || 'system_state',
-};
-
-export default class SystemState extends EventEmitter {
-  constructor(redis, rabbitmqChannel) {
-    super();
-
-    this.redis = redis;
-    this.pubsub = new RabbitMQPubSub(rabbitmqChannel, {
-      exchangeName: config.exchangeName,
-      queueNamePrefix: config.queueNamePrefix,
-    });
-  }
-
-  async setup() {
-    await this.pubsub.setup();
-
-    this.pubsub.on('*', (message) => {
-      const stateName = message.eventName;
-      const body = message.data;
-
-      debug(`PubSub new state "${stateName}"`);
-
-      this.emit(stateName, body.value, body);
-      this.emit('*', stateName, body.value, body);
-    });
-  }
-
-  get(name) {
-    debug(`GET: "${name}"`);
-
-    return new Promise((resolve, reject) => {
-      this.redis.get(name, (err, reply) => {
-        if (err) {
-          return reject(err);
-        }
-
-        if (reply) {
-          const json = JSON.parse(reply);
-
-          return resolve(json.value);
-        }
-
-        return resolve(reply);
-      });
-    });
-  }
-
-  set(name, value) {
-    debug(`SET: "${name}"`);
-    const body = {
-      value,
-      timestamp: Math.floor(Date.now() / 1000),
-      processId: process.pid,
-      processName: process.argv0,
-    };
-
-    this.redis.set(name, JSON.stringify(body));
-    this.pubsub.publish(name, body);
-  }
-
-  all() {
-    return new Promise((resolve, reject) => {
-      this.redis.keys('*', async (err, keys) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const results = keys.map(key => this.get(key));
-
-        return resolve(await Promise.all(results));
-      });
-    });
-  }
+  return systemState;
 }
